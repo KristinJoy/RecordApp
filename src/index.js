7719d62f50b0733e258c59d4999fbd0f4086bad5
Jamie Iguchi <jamie@Jamies-MacBook-Pro.local>
2018-11-01 18:37:30 -0600

    Fixed the JSON link issue. App works great now, just need to format list of record data and fix images.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Data from './records.json';
 
 // -------------------------------------------
-// Main Component (Card Container; Card Body)
+// App Component
+// -------------------------------------------
+class App extends React.Component {
+  render() {
+    return(<Deck/>)
+  }
+}
+
+// -------------------------------------------
+// Deck Component to parse through JSON
+// -------------------------------------------
+
+class Deck extends React.Component {
+  render() {
+      let result = [];
+      for (var i = 0; i < Data.records.length; i++) {
+        result.push(<Card record={Data.records[i]}/>);
+      }
+      return(result)
+  }
+}
+
+// -------------------------------------------
+// Main Component
 // -------------------------------------------
 
 class Card extends React.Component {
@@ -11,14 +35,19 @@ class Card extends React.Component {
     return(
       <div className='card-container'>
         <div className='card-body'>
-          <CardBack />
+          <CardBack record={this.props.record} />
 
-          <CardFront />
+
+          <CardFront record={this.props.record} />
         </div>
       </div>
     )
   }
 }
+
+// Adding the following code snippet to CardBack to try to link to JSON Data will just break the whole app:
+// <h3 className="title">{this.props.record.title}</h3>
+// Returns a "TypeError: Cannot read property 'title' of undefined"
 
 // -------------------------------------------
 // Front Side Component
@@ -30,9 +59,10 @@ class CardFront extends React.Component {
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-xs-6'>
-              <img src='https://rosequarter.com/wp-content/uploads/2017/07/17Odesza1080X1080_v1.jpg' />
+              <img className="image" src={this.props.record.art}/>
             </div>
               <div className='col-xs-6 side-front-content'>
+
             </div>
           </div>
         </div>
@@ -49,7 +79,7 @@ class CardBack extends React.Component {
     return(
       <div className='card-side side-back'>
         <div className='container-fluid'>
-          <h1>Album Info</h1>
+          <h3 className="title">{this.props.record.title}</h3>
           <p>Put stuff here</p>
         </div>
       </div>
@@ -58,6 +88,6 @@ class CardBack extends React.Component {
 }
 
 ReactDOM.render(
-  <Card />,
+  <App />,
   document.getElementById('root')
 );
